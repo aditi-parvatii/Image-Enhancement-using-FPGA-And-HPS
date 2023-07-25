ddc92b1537553be7c766de6269804ddbd813d9ea
aditi-parvatii <101183870+aditi-parvatii@users.noreply.github.com>
2023-07-25 10:13:58 +0530

    Add files via upload

---
+++ operation.v
@@ -0,0 +1,7 @@
+`define Inputfile "hexop.hex" 
+`define Outputfile "output2.bmp"
+		
+//`define CONTRAST_OPERATION
+//`define BRIGHTNESS_OPERATION
+//`define INVERT_OPERATION
+`define THRESHOLD_OPERATION 
